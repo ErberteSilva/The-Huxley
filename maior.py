6551885c296fd754c6abbb410b621ee65c19bdbc
ErberteSilva <87332804+ErberteSilva@users.noreply.github.com>
2021-10-02 00:45:23 -0300

    att

--- maior.py
+++ maior.py
@@ -1,12 +1,12 @@
 M = int(input())
 N = int(input())
-m = 0
-v = 0
-while m <= N :
-    if m % M == 0 :
-        v = m 
-    m = m+1 
-if v != M :
-    print(v)
-else :
+m = M
+v = 1
+if M*v <=N: 
+    while M*v <=N: 
+       m = M
+       m = m*v
+       v = v+1
+else:
     print("sem multiplos menores que",N)
+print(m)
